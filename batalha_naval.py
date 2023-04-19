622782e8d9534be03321391ad3f1d7d052a4d1ac
Rkzzin <henriquels5@al.insper.edu.br>
2023-04-19 16:10:36 -0300

    Adição da primeira função

--- batalha_naval.py
+++ batalha_naval.py
@@ -1,3 +1,19 @@
-# Funções
+## Funções ##
+
+# Define as posições do navio de acordo com a orientação
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    posicoes = []
+
+    # Define as posições caso a orientação seja horizontal
+    if orientacao == 'horizontal':
+        for i in range(tamanho):
+            posicoes.append([linha, coluna + i])
+    
+    # Define as posições caso a orientação seja vertical
+    elif orientacao == 'vertical':
+        for i in range(tamanho):
+            posicoes.append([linha + i, coluna])
+
+    return posicoes
 
 # Loop do jogo
