aaedb64595eb4230ebc16d05d15b2eacd2bb1bee
Rkzzin <henriquels5@al.insper.edu.br>
2023-04-19 16:29:59 -0300

    Adição da segunda função

---
+++ Funções separadas/preenche_frota.py
@@ -0,0 +1,12 @@
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+
+    # Verifica se o navio já está na frota
+    if nome_navio not in frota:
+        # Caso não esteja, adiciona o navio à frota
+        frota[nome_navio] = [define_posicoes(linha, coluna, orientacao, tamanho)]
+
+    # Senão, adiciona as posições do tipo de navio à frota    
+    else:
+        frota[nome_navio] += [define_posicoes(linha, coluna, orientacao, tamanho)]
+
+    return frota
--- batalha_naval.py
+++ batalha_naval.py
@@ -16,4 +16,20 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
 
     return posicoes
 
-# Loop do jogo
+
+# Preenche a frota com os navios
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+
+    # Verifica se o navio já está na frota
+    if nome_navio not in frota:
+        # Caso não esteja, adiciona o navio à frota
+        frota[nome_navio] = [define_posicoes(linha, coluna, orientacao, tamanho)]
+
+    # Senão, adiciona as posições do tipo de navio à frota    
+    else:
+        frota[nome_navio] += [define_posicoes(linha, coluna, orientacao, tamanho)]
+
+    return frota
+
+
+## Loop do jogo ##
