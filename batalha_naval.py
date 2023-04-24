3923f94229c843c56a43131cefa312fca5180b0c
Enzo <enzopj@al.insper.edu.br>
2023-04-24 11:05:37 -0300

    Função 'faz-jogada': Faz a jogada e confere se acertou

--- batalha_naval.py
+++ batalha_naval.py
@@ -31,5 +31,13 @@ def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
 
     return frota
 
+# Faz a jogada e confere se acertou
+def faz_jogada(tabuleiro,linha,coluna):
+    if tabuleiro[linha][coluna]==1:
+        tabuleiro[linha][coluna]='X'
+    else:
+        tabuleiro[linha][coluna]='-'
+    return tabuleiro
+
 
 ## Loop do jogo ##
